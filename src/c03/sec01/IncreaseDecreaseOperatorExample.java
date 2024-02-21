04489055b8cd535c12cdfb86f536052832747b2c
jaewook <wodnr5050@gmail.com>
2024-02-21 16:46:32 +0900

    20240210 수

--- src/c03/sec01/IncreaseDecreaseOperatorExample.java
+++ src/c03/sec01/IncreaseDecreaseOperatorExample.java
@@ -6,28 +6,28 @@ public class IncreaseDecreaseOperatorExample {
         int y = 10;
         int z;
 
-        x++;
-        ++x;
+        x++;            //11
+        ++x;            //12
         System.out.println("x = " + x);
 
         System.out.println("-----------------------------");
-        y--;
-        --y;
+        y--;                //9
+        --y;                //8
         System.out.println("y = " + y);
 
         System.out.println("---------------------------");
-        z = x++;
+        z = x++;        //z = 12   x=  13
         System.out.println("z = " + z);
         System.out.println("x = " + x);
 
         System.out.println("---------------------------");
 
-        z = ++x;
+        z = ++x;     //z =  13   x = 14
         System.out.println("z = " + z);
         System.out.println("x = " + x);
 
         System.out.println("---------------------------");
-        z = ++x + y++;
+        z = ++x + y++;      //z = 23    x = 15   y = 9
         System.out.println("z = " + z);
         System.out.println("x = " + x);
         System.out.println("y = " + y);
