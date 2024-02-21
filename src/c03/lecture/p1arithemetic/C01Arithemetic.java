27c6b42610e95fd2b44df04d68ac6a5178121ed7
jaewook <wodnr5050@gmail.com>
2024-02-21 16:39:17 +0900

    20240210 수

---
+++ src/c03/lecture/p1arithemetic/C01Arithemetic.java
@@ -0,0 +1,20 @@
+package c03.lecture.p1arithemetic;
+
+public class C01Arithemetic {
+    public static void main(String[] args) {
+        // 산술 연산자
+        // +, -, *, / , %
+        // 피연산자 : numeric(정수, 실수, 문자)
+
+
+        int a = 3;
+        int b = 9;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+    }
+}
