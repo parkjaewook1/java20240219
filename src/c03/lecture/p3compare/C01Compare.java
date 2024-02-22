0464e1785dab44f2c4cc3d5f6927b0025f1b4fb6
jaewook <wodnr5050@gmail.com>
2024-02-22 15:48:40 +0900

    20240222 목

---
+++ src/c03/lecture/p3compare/C01Compare.java
@@ -0,0 +1,27 @@
+package c03.lecture.p3compare;
+
+public class C01Compare {
+    public static void main(String[] args) {
+        // 비교 연산자
+        // ==(같다), !=(같지 않다), >, >=, <, <=
+        // 연산결과 : boolean (true , false)
+        // 다른 타입간 연산 가능 (하지만 가능하면 같은 타입끼리 연산)
+
+        int a = 5;
+        int b = 8;
+
+        boolean b1 = a == b;
+        System.out.println("b1 = " + b1);
+
+        boolean b2 = a != b;
+        System.out.println("b2 = " + b2);
+
+        boolean b3 = a >= b;
+        System.out.println("b3 = " + b3);
+
+        boolean b4 = a <= b;
+        System.out.println("b4 = " + b4);
+
+
+    }
+}
