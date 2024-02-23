42cf99d1d1747219c204475f55c9ff549eacc49b
jaewook <wodnr5050@gmail.com>
2024-02-23 10:46:29 +0900

    20240223 금

---
+++ src/ch04/lecture/p1if/C03ElseIf.java
@@ -0,0 +1,27 @@
+package ch04.lecture.p1if;
+
+public class C03ElseIf {
+    public static void main(String[] args) {
+
+
+        if (true) {
+            System.out.println("statement 1");
+        } else if (true) {
+            System.out.println("statement 2");
+        } else if (true) {
+            System.out.println("statement 3");
+        } else if (true) {
+            System.out.println("statement 4");
+        } else if (true) {
+            System.out.println("statement 5");
+        } else if (true) {
+            System.out.println("statement 6");
+        } else if (true) {
+            System.out.println("statement 7");
+        } else if (true) {
+            System.out.println("statement 8");
+        } else if (true) {
+            System.out.println("statement 9");
+        }
+    }
+}
