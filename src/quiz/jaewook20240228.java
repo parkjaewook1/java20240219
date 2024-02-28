e0dfd3e44f705a06169b7a025fc21c25ac78a81a
jaewook <wodnr5050@gmail.com>
2024-02-28 18:18:13 +0900

    20240227 화

--- src/quiz/jaewook20240228.java
+++ src/quiz/jaewook20240228.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class jaewook20240228 {
     public static void main(String[] args) {
         String[] strlist = {"operation", "cwal", "show me the money"};
-        int[] answer = {};
+        int[] answer = {};                  //answer[0] answer[1] answer[2]         = answer[i]     strlist.length
         answer = new int[strlist.length];
 
         for (int i = 0; i < strlist.length; i++) {
