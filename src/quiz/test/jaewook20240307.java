48075aba1c5412aba67ab96e9d612417dbaa9b00
jaewook <wodnr5050@gmail.com>
2024-03-07 18:51:05 +0900

    20240307 목

---
+++ src/quiz/test/jaewook20240307.java
@@ -0,0 +1,13 @@
+package quiz.test;
+
+public class jaewook20240307 {
+    public static void main(String[] args) {
+        int n = 13;
+        int k = 1;
+        int answer = 0;
+        answer = (12000 * n) + (k - n / 10) * 2000;
+
+
+        System.out.println(answer);
+    }
+}
